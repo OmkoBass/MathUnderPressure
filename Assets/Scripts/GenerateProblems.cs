93ea478b3ad558ebe19889cf36ec78c98178e63b
unknown <omkob@hotmail.com>
2021-11-08 15:54:40 +0100

    Resets score when you restart the level

--- Assets/Scripts/GenerateProblems.cs
+++ Assets/Scripts/GenerateProblems.cs
@@ -7,6 +7,8 @@ public class GenerateProblems : MonoBehaviour
     private int zPosition = 15;
     private void Start()
     {
+        GameManager.Score = 0;
+
         for (int i = 0; i < 3; i++)
         {
             Instantiate(Problem, new Vector3(0, 0, zPosition), Quaternion.identity);
